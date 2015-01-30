79572360b32b8fb30a9c3121438d02779645a5d8
GitHubGuy119 <grant.gilbert119@gmail.com>
2015-01-29 20:02:35 -0800

    Created InitDefaultCommand in Craaaw subsystem
    Did not have InitDefaultCommand method--added that. Also edited the
    define in AutoCanerino subsystem.

--- Cyclophosphamide/src/Subsystems/Craaaw.cpp
+++ Cyclophosphamide/src/Subsystems/Craaaw.cpp
@@ -8,14 +8,16 @@ Craaaw::Craaaw(): Subsystem("Craaaw"){
 	//liftereeno = new LIFT_MOTOR_TYPE(LIFT_MOTOR_CAN);
 	canDetected = new DigitalInput(CRAAAW_CAN_DETECTOR);
 	craaawLocker = new Solenoid(CRAAAW_TOGGLE);
-	SetDefaultCommand(new CraaawActuate());
-
 }
 
 Craaaw::~Craaaw(){
 	delete canDetected;
 	delete craaawLocker;
 
+}
+
+void Craaaw::InitDefaultCommand(){
+	SetDefaultCommand(new CraaawActuate());
 }
 
 bool Craaaw::getInput(){
