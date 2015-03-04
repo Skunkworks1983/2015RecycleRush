667919056b4517a4c60ffc4516fa2d7dcc8c3921
GitHubGuy119 <grant.gilbert119@gmail.com>
2015-03-03 18:20:33 -0800

    Changed a typo

--- Cyclophosphamide/src/Subsystems/CanCollecterino.cpp
+++ Cyclophosphamide/src/Subsystems/CanCollecterino.cpp
@@ -16,7 +16,7 @@ CanCollecterino::CanCollecterino() :
 
 	SAFE_INIT(CAN_SENSOR_PORT, canSensor = new DigitalInput(CAN_SENSOR_PORT););
 
-		 = new PIDController(CAN_ARM_P, CAN_ARM_I, CAN_ARM_D, liftPot,
+		armPID = new PIDController(CAN_ARM_P, CAN_ARM_I, CAN_ARM_D, liftPot,
 			new StallableMotor(liftPot, 0.8, liftMotorRight, liftMotorLeft));
 	armPID->SetOutputRange(-.7, .7);
 	armPID->SetInputRange(CAN_POT_DOWN_POSITION, CAN_POT_UP_POSITION);
