fcbd81b92e094d0890c02a2886020335fbe07641
DavidDupre <djdupre7@gmail.com>
2015-03-05 20:10:52 -0800

    Revert "Virtual OI in smartdashboard :radio_button:"
    This reverts commit 095a8c5e8d2863402c681fbf50a170a14197652c.

--- Cyclophosphamide/src/OI.h
+++ Cyclophosphamide/src/OI.h
@@ -42,8 +42,6 @@ public:
 	bool getUnactuate();
 	void registerButtonListeners();
 	bool isJoystickButtonPressed(bool isLeft, int val);
-	void createButton(std::string key, Button *b, Command *c);
-	void createSwitch(std::string key, Button *b, Command *on, Command *off);
 };
 
 #endif
