abff6edbca899e40e0283eb6cdb05f95fac693ba
DavidDupre <djdupre7@gmail.com>
2015-03-11 21:48:11 -0700

    Speed PID on tote intake
    Separate branch so I don't break comp code / get yelled at.

--- Cyclophosphamide/src/Subsystems/OldToteIntakerino.cpp
+++ Cyclophosphamide/src/Subsystems/OldToteIntakerino.cpp
@@ -7,6 +7,13 @@
 #include <chrono>
 #include <cmath>
 
+// TODO tune these
+// TODO move these to RobotMap.h
+#define TOTE_INTAKE_RPM 10
+#define TOTE_INTAKE_P 0.005
+#define TOTE_INTAKE_I 0.0
+#define TOTE_INTAKE_D 0.0
+
 OldToteIntakerino::OldToteIntakerino() :
 		Subsystem("OldToteIntakerino") {
 	SAFE_INIT(TOTE_INTAKE_MOTOR_PORT_OLD,
@@ -15,10 +22,7 @@ OldToteIntakerino::OldToteIntakerino() :
 	pid = new PIDController(TOTE_INTAKE_P, TOTE_INTAKE_I, TOTE_INTAKE_D,
 			encoder, this);
 	pid->SetOutputRange(-TOTE_INTAKE_PID_FULL, TOTE_INTAKE_PID_FULL);
-	//pid->Enable();
-	hasTote = false;
-	isWindingDown = false;
-	startAutoTime = getTime();
+	pid->Enable();
 }
 
 void OldToteIntakerino::InitDefaultCommand() {
@@ -26,72 +30,28 @@ void OldToteIntakerino::InitDefaultCommand() {
 }
 
 void OldToteIntakerino::PIDWrite(float output) {
-	/*
-	SmartDashboard::PutNumber("intake PID output", output);
-	if(isWindingDown && fabs(output) < TOTE_INTAKE_DETECTION_THRESHOLD){
-		isWindingDown = false;
-	} else if (fabs(output) > TOTE_INTAKE_DETECTION_THRESHOLD) {
-		hasTote = true;
-		startAutoTime = getTime();
-	}
-	if(hasTote){
-		toteIntakeMotor->Set(TOTE_INTAKE_MOTOR_FULL);
-		if(getTime() > startAutoTime + TOTE_INTAKE_AUTO_TIME_MS){
-			hasTote = false;
-			hold();
-		}
-		// if timed out hasTote = false
-	} else {
-		if (output < 0) {
-			output = 0;
-		}
-		toteIntakeMotor->Set(-output);
-	}
-	*/
-
-	if (output < 0) {
-		output = 0;
-	}
-	toteIntakeMotor->Set(-output);
+	toteIntakeMotor->Set(output);
 }
 
 double OldToteIntakerino::PIDGet() {
-	return encoder->Get() * TOTE_INTAKE_ENCODER_TICKS_PER_REV;
+	return getRPM();
+}
+
+double OldToteIntakerino::getRPM() {
+	return encoder->Get() / TOTE_INTAKE_ENCODER_TICKS_PER_REV * 60.0;
 }
 
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
-void OldToteIntakerino::hold() {
-	encoder->Reset();
-	pid->SetSetpoint(0);
-	pid->Enable();
-}
-
 void OldToteIntakerino::setMotor(float speed) {
-	pid->Disable();
-	toteIntakeMotor->Set(speed);
-	if (fabs(speed) > 0) {
-		hasTote = false;
-		isWindingDown = true;
-	}
+	pid->SetSetpoint(speed);
 }
 
 Encoder *OldToteIntakerino::getEncoder() {
 	return encoder;
 }
 
-bool OldToteIntakerino::isLoaded() {
-	return hasTote;
-}
-
 PIDController *OldToteIntakerino::getPID() {
 	return pid;
-}
-
-unsigned long OldToteIntakerino::getTime() {
-	using namespace std::chrono;
-	unsigned long ms = duration_cast<milliseconds>(
-			high_resolution_clock::now().time_since_epoch()).count();
-	return ms;
 }
