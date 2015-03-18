6012e8bb0119e33bdc18c81dace64562d96a91ac
Connor Barlow <conn.barlowr@gmail.com>
2015-03-18 11:32:31 -0700

    Guess it didn't commit everything
    This is rest

--- Cyclophosphamide/src/Commands/CanCollecterino/Arms/Whack.cpp
+++
@@ -1,9 +0,0 @@
-#include "Whack.h"
-#include "MoveArms.h"
-#include "MoveWrist.h"
-
-Whack::Whack() {
-	AddParallel(new MoveArms(CAN_POT_KNOCK_POSITION));
-	AddSequential(new WaitCommand(.3));
-	AddSequential(new MoveWrist(MoveWrist::close));
-}
--- Cyclophosphamide/src/Commands/CanCollecterino/Collect.cpp
+++
@@ -1,6 +0,0 @@
-#include <Commands/CanCollecterino/Collect.h>
-
-Collect::Collect(Induct::State direction, MoveWrist::State state) {
-	AddSequential(new MoveWrist(state));
-	AddSequential(new Induct(direction));
-}
