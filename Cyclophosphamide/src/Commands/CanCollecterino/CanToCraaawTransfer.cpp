612eac422ff6838e2b693315b7007d72d7da6aaf
DavidDupre <djdupre7@gmail.com>
2015-03-18 11:12:54 -0700

    Intake hotfix
    Gotta be safe or the tote intake won't run.

--- Cyclophosphamide/src/Commands/CanCollecterino/CanToCraaawTransfer.cpp
+++ Cyclophosphamide/src/Commands/CanCollecterino/CanToCraaawTransfer.cpp
@@ -1,15 +1,17 @@
 #include <Commands/CanCollecterino/Arms/Induct.h>
+#include <Commands/CanCollecterino/Arms/MoveArms.h>
 #include <Commands/CanCollecterino/Arms/MoveWrist.h>
 #include <Commands/CanCollecterino/CanToCraaawTransfer.h>
 #include <Commands/CanCollecterino/Craaaw/CraaawActuate.h>
-#include <Commands/ToteLifting/LiftToHeight.h>
+#include <Commands/ToteLifting/SafeLiftToHeight.h>
 #include <Commands/WaitCommand.h>
 #include <RobotMap.h>
 
 CanToCraaawTransfer::CanToCraaawTransfer() {
+	AddSequential(new MoveArms(CAN_POT_UP_POSITION));
 	AddSequential(new Induct(Induct::forward, .5));
 	AddSequential(new CraaawActuate(CraaawActuate::close));
 	AddSequential(new WaitCommand(.25));
 	AddSequential(new MoveWrist(MoveWrist::open));
-	AddSequential(new LiftToHeight(TOTE_LIFTER_LOAD_HEIGHT));
+	AddSequential(new SafeLiftToHeight(TOTE_LIFTER_LOAD_HEIGHT));
 }
