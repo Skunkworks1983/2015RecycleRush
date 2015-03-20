654fd370610893626b3372729ffe142bc9015dbc
Ross <r.bajocich@gmail.com>
2015-03-19 18:33:25 -0700

    Fixes Build

--- Cyclophosphamide/src/Commands/Armerino/Arms/MoveWrist.cpp
+++ Cyclophosphamide/src/Commands/Armerino/Arms/MoveWrist.cpp
@@ -19,7 +19,7 @@ void MoveWrist::Initialize() {
 		break;
 	case open:
 		// TODO is it sketchy to use two subsystems?
-		if(!override && !armLifter->getSetpoint()>CAN_POT_DOWN_POSITION){
+		if(override || !armLifter->getSetpoint()>CAN_POT_DOWN_POSITION){
 			armWristerino->setWrist(DoubleSolenoid::kForward);
 		}
 		break;
